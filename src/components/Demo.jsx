87f0d5301a19ddc61f3e3222e94509869c824c0c
Ahmed <oublihi.a@gmail.com>
2023-05-12 18:45:26 +0200

    Demo wünsch

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import React, { useState } from "react";
+
+import { linkIcon } from "../assets";
 
 const Demo = () => {
-  return (
-    <div>Demo</div>
-  )
-}
+  const [article, setArticle] = useState({
+    url: "",
+    summary: "",
+  });
 
-export default Demo
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <section className='mt-16 w-full max-w-xl'>
+      {/* Search */}
+      <div className='flex flex-col w-full gap-2'>
+        <form
+          className='relative flex justify-center items-center'
+          onSubmit={handleSubmit}
+        >
+          <img
+            src={linkIcon}
+            alt='link-icon'
+            className='absolute left-0 my-2 ml-3 w-5'
+          />
+
+          <input
+            type='url'
+            placeholder='Paste the article link'
+            value={article.url}
+            onChange={(e) => setArticle({ ...article, url: e.target.value })}
+            required
+            className='url_input peer'
+          />
+          <button
+            type='submit'
+            className='submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700 '
+          >
+            <p>↵</p>
+          </button>
+        </form>
+
+        {/* Browse History */}
+      </div>
+    </section>
+  );
+};
+
+export default Demo;
